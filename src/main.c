982986ebe3445d3e1125c4b43c2497f6ec856c1f
Jithin Renji <jithin0904@gmail.com>
2026-04-26 20:10:17 -0600

    main(): Accept env vars

--- src/main.c
+++ src/main.c
@@ -68,10 +68,9 @@ void clara_init(void)
     }
 }
 
-int main(int argc, const char *argv[])
+int main(int argc, const char *argv[], const char *envp[])
 {
     clara_init();
-
     if (argc == 2 && strcmp(argv[1], "--debug") == 0)
         yydebug = 1;
 
